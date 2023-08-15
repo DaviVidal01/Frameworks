26f36c873446b11026aa1ab0068e5628e9c0042d
DaviVidal01 <davividal0605@gmail.com>
2023-08-14 22:21:14 -0300

    Entendendo conceitos de Mysql e criando meu primeiro Banco de Dados

--- Django/Framework-6/WebGamer/Cyborg/models.py
+++ Django/Framework-6/WebGamer/Cyborg/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Teste(models.Model):
+    nome_card = models.CharField(max_length = 200)
+    description = models.TextField()
+    path = models.TextField()
+    primeiro_texto = models.TextField()
+    segundo_texto = models.TextField()
+    alunos = models.IntegerField()
+    date_create = models.DateTimeField(default = datetime.now, blank = True)
+
+class NovaTabela(models.Model):
+    titulo = models.CharField(max_length = 255)
+    descricao = models.TextField()
+    imagem = models.ImageField()
+    pontos = models.DecimalField(max_digits=4, decimal_places=1 )
