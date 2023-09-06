562eb121a62edfd4efdddd47e9e93714de30dda0
DaviVidal01 <davividal0605@gmail.com>
2023-09-05 21:54:44 -0300

    Criando uma venv e realizando mais um exercício de Framework

--- Django/Framework-10/Feane/Website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('sobre/', views.sobre, name="sobre"),
-    path('menu/', views.menu, name="menu"),
-    path('livro/', views.livro, name="livro")
-]
